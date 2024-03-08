a38dfeeefde46aac244f46136b44f794c0fed642
syhskin <94190411+syhskin@users.noreply.github.com>
2024-03-09 03:02:15 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 set -eux
-
 GKI_ROOT=$(pwd)
-
 echo "[+] GKI_ROOT: $GKI_ROOT"
-
 if test -d "$GKI_ROOT/common/drivers"; then
      DRIVER_DIR="$GKI_ROOT/common/drivers"
 elif test -d "$GKI_ROOT/drivers"; then
@@ -14,7 +11,6 @@ else
      echo '[+] You should modify this script by yourself.'
      exit 127
 fi
-
 git clone https://github.com/tiann/KernelSU
 cd "$GKI_ROOT/KernelSU"
 git stash
@@ -28,10 +24,8 @@ else
     git checkout "$1"
 fi
 cd "$GKI_ROOT"
-
 echo "[+] GKI_ROOT: $GKI_ROOT"
 echo "[+] Copy kernel su driver to $DRIVER_DIR"
-
 cd "$DRIVER_DIR"
 if test -d "$GKI_ROOT/common/drivers"; then
      ln -sf "../../KernelSU/kernel" "kernelsu"
@@ -39,12 +33,9 @@ elif test -d "$GKI_ROOT/drivers"; then
      ln -sf "../KernelSU/kernel" "kernelsu"
 fi
 cd "$GKI_ROOT"
-
 echo '[+] Add kernel su driver to Makefile'
-
 DRIVER_MAKEFILE=$DRIVER_DIR/Makefile
 DRIVER_KCONFIG=$DRIVER_DIR/Kconfig
 grep -q "kernelsu" "$DRIVER_MAKEFILE" || printf "\nobj-\$(CONFIG_KSU) += kernelsu/\n" >> "$DRIVER_MAKEFILE"
 grep -q "kernelsu" "$DRIVER_KCONFIG" || sed -i "/endmenu/i\\source \"drivers/kernelsu/Kconfig\"" "$DRIVER_KCONFIG"
-
 echo '[+] Done.'
